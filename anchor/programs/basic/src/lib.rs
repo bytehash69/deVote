34316a0b95ee440ade791b19e4fba624afd73384
Adiflickzzz <adivardhan2248@gmail.com>
2025-07-11 02:29:56 +0530

    Added accounts data struct

--- anchor/programs/basic/src/lib.rs
+++ anchor/programs/basic/src/lib.rs
@@ -5,12 +5,32 @@ declare_id!("JAVuBXeBZqXNtS73azhBDAoYaaAFfo4gWXoZe2e7Jf8H");
 #[program]
 pub mod basic {
     use super::*;
-
-    pub fn greet(_ctx: Context<Initialize>) -> Result<()> {
-        msg!("GM!");
-        Ok(())
-    }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[account]
+#[derive(InitSpace)]
+pub struct Poll {
+    pub poll_id: u64,
+    pub owner: Pubkey,
+    pub start_time: u64,
+    pub end_time: u64,
+    #[max_len(200)]
+    pub question: String
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Option {
+    pub option_id: u64,
+    pub poll_id: u64,
+    #[max_len(60)]
+    pub title: String
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct VoteRecord {
+    pub has_voted: bool,
+    pub option_id: u64,
+    pub pool_id: u64
+}
